36d47890436b03fb6a56a1a8143783488553f5b0
vianalin <vlin21@stuy.edu>
2020-12-07 14:01:25 -0500

    reset

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,12 +1,19 @@
 import java.util.NoSuchElementException;
 import java.util.ArrayList;
 
-public class ArraySequence implements IntegerSequence{
-  int currentIndex;
-  int[] data;
+public class ArraySequence implements IntegerSequence {
+	int currentIndex;
+	int[] data;
 
-  /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int[] other){
-  	data = other.clone();
-  }
+	//public ArraySequence(int[] other) {
+		//data = other.clone();
+	//}
+
+	//public ArraySequence(IntegerSequence otherseq){	}
+
+	public void reset() {
+		currentIndex = 0;
+	}
+
+	
 }
