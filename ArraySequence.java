0233cb5ccaf8dd2a10e18e0bb2065a8c267db0f2
vianalin <vlin21@stuy.edu>
2020-12-07 13:49:46 -0500

    arraysequence

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,8 +1,12 @@
 import java.util.NoSuchElementException;
+import java.util.ArrayList;
+
 public class ArraySequence implements IntegerSequence{
   int currentIndex;
-  int []data;
+  int[] data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int[] other){
+  	data = other.clone();
+  }
 }
