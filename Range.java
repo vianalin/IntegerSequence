fefdbcd533259e5df3aa159a6ab31a41096012a9
vianalin <vlin21@stuy.edu>
2020-12-06 19:49:16 -0500

    reset

---
+++ Range.java
@@ -0,0 +1,18 @@
+import java.util.NoSuchElementException;
+public class Range implements IntegerSequence{
+  private int start, end, current;
+  public Range(int start, int end){
+    this.start = start;
+    this.end = end;
+    current = start;
+  }
+  public void reset(){
+    current = start;
+  }
+  //public int length(){  }
+  //public boolean hasNext(){  }
+
+  //@throws NoSuchElementException
+  //public int next(){}
+  
+}
