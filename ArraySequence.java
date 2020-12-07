a80b27d11f5575ec0aa3b84a25d7dc6d09cd06a9
vianalin <vlin21@stuy.edu>
2020-12-07 13:35:24 -0500

    base code

---
+++ ArraySequence.java
@@ -0,0 +1,8 @@
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence{
+  int currentIndex;
+  int []data;
+
+  /*Construct the sequence by copying values from the other array into the data array*/
+  public ArraySequence(int [] other){  }
+}
