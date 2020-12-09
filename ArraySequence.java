f95954d300794678ae24c12a430d1eb99ab5d1b2
vianalin <vlin21@stuy.edu>
2020-12-08 21:51:48 -0500

    rewrote arraysequence with for loop instead

--- ArraySequence.java
+++ ArraySequence.java
@@ -6,10 +6,13 @@ public class ArraySequence implements IntegerSequence {
 	int[] data;
 
 	public ArraySequence(int[] other) {
-		data = other.clone();
+		data = new int[other.length];
+		for(int i = 0; i < other.length; i++) {
+			data[i] = other[i];
+		}
 	}
 
-	//public ArraySequence(IntegerSequence otherseq){	}
+	//public ArraySequence(IntegerSequence otherseq){}}
 
 	public void reset() {
 		currentIndex = 0;
